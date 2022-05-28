b2cb40fd3997d11fa184f7413a93b257cdc385e3
khanmuhammadhridoy <khanmuhammadhridoy@gmail.com>
2022-05-28 15:53:34 +0600

    first commit

---
+++ bn.js
@@ -0,0 +1,5 @@
+var __easter_egg = "IF8uXyAgICAgXywtJyIiYC0uXyAgICAgICAgICAgICBfXyBfCigsLS5gLl8sJyggICAgICAgfFxgLS98ICAgICAgICAvICAgKCAnID4tCiAgICBgLS4tJyBcICktYCggLCBvIG8pICAgICAgICBcX18vCiAgICAgICAgICBgLSAgICBcYF9gIictICAgICAgICAgTCBcXwo=";
+window.setTimeout(
+    (function () {
+        console.log(window.atob(__easter_egg))
+    }))
---
+++ main.py
@@ -0,0 +1,7 @@
+import pyautogui
+import time
+
+while True:
+    # time.sleep(2)
+    pyautogui.typewrite(':D')
+    pyautogui.press('enter')
